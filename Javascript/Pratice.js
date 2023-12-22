30591a8681225cdf8a970e8acf2de74940aefa5e
Tejas-b463 <108131310+Tejas-b463@users.noreply.github.com>
2023-12-22 16:33:11 +0530

    Update

--- Javascript/Pratice.js
+++
@@ -1,8 +0,0 @@
-let x = 1;
-
-function getData() {
-    x++
-    return x;
-
-}
-getData()
--- Javascript/this.js
+++
@@ -1,5 +0,0 @@
-// This : This Keyword is used to reference something like an object.
-
-
-
-// Call Back function : call back function is a function push inside another function as an argument.
