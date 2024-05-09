782ab685f6394ab88cdb356794e68c9dcea3d68f
Tejas-b463 <108131310+Tejas-b463@users.noreply.github.com>
2024-05-09 11:04:15 +0530

    find key in object

---
+++ test-js/Logical/for.js
@@ -0,0 +1,14 @@
+const obj = {
+    name: 'tejas',
+    age: 23,
+    profession: 'frontend developer'
+}
+
+
+for (let key in obj) {
+    console.log(key);
+}
+
+for (let key in obj) {
+    console.log(obj(key))
+}
