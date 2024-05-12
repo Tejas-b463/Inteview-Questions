6e4d644f67410406460e94985d385457f3224872
Tejas-b463 <108131310+Tejas-b463@users.noreply.github.com>
2024-05-12 11:10:54 +0530

    polyfills for filter method

--- test-js/Polyfills.js
+++ test-js/Polyfills.js
@@ -15,3 +15,23 @@ const mapping = arr.myMap((arr) => {
 })
 
 console.log(mapping)
+
+// polyfills for filters
+
+Array.prototype.myFilter = function(cb) {
+    let temp = []
+    for (let i = 0; i < this.length; i++) {
+        if (cb(this[i], i, this)) {
+            temp.push(this[i]);
+        }
+    }
+    return temp;
+}
+
+const arr1 = [1, 2, 3, 4]
+
+const filtering = arr1.myMap((arr) => {
+    return arr * 2
+})
+
+console.log(filtering)
